1f7f520bc8c4888f425af07d4bbc82305f4aa1e6
Ed Scheinerman <ers@jhu.edu>
2021-09-23 12:05:12 -0400

    Corrected, but possibly new vertex set

--- src/graph_square.jl
+++ src/graph_square.jl
@@ -1,16 +1,14 @@
-using SimpleGraphs
-function graph_square(G::SimpleGraph{T}) where T
-    H = SimpleGraph{T}()
-    for v in G.V 
-        add(H,v)
-    end
 
-    for v in G.V 
-        for w in G.V 
-            if dist(G,v,w) == 2 
-                add!(H,v,w)
-            end
-        end
-    end
-    return H
+
+using SimpleGraphs
+
+"""
+    graph_square(G::SimpleGraph)
+Create a new graph in which two vertices are adacent if there is a two-step path 
+between them in `G`.
+"""
+function graph_square(G::SimpleGraph{T}) where T
+    A = adjacency(G)
+    AA = A^2 
+    return SimpleGraph(AA)
 end
